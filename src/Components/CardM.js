067ee4c4741ea3ec415011c87ac598e779817937
abinash1415 <obiabinash2@gmail.com>
2024-04-26 10:55:34 +0200

    modal

--- src/Components/CardM.js
+++ src/Components/CardM.js
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from 'react';
+import DetailsModal from './ModalM';
 
 const CardM=(movie)=>{
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const openModal = () => setModalIsOpen(true);
+    const closeModal = () => setModalIsOpen(false);
+
     console.log(movie.info)
+    
     let img_path="https://image.tmdb.org/t/p/w500"
+    
     return(
         <>
-            <div className="movie">
-                <img src={img_path + movie.info.poster_path} className="poster"></img>
+            {/*Poster*/}
+            <div className="movie" onClick={openModal} >
+                <img src={img_path + movie.info.poster_path} alt="Erreur" className="poster"></img>
                 <div className="movie-details">
                     <div className="box">
                         <h4 className="title">{movie.info.title}</h4>
                         <p className="rating">{movie.info.vote_average.toFixed(1)}</p>
                     </div>
-                    <div className="overview">
-                        <h1>Détails</h1>
-                        {movie.info.overview}
-                    </div>
                 </div>
+            </div>
+            {/*Modal*/}
+            <div>
+                <DetailsModal isOpen={modalIsOpen} onRequestClose={closeModal}>
+                    <img src={img_path + movie.info.poster_path} alt="Erreur" className="poster"></img>
+                    <div className="movie-details">
+                        <div className="box">
+                            <h4 className="title">{movie.info.title}</h4>
+                            <h3 className='#'>{movie.info.release_date}</h3>
+                            <p className="rating">{movie.info.vote_average.toFixed(1)}</p>
+                            <div className='overview'><h1>Synopsis</h1>{movie.info.overview}</div>
+                        </div>
+                    </div>
+                </DetailsModal>
             </div>
         </>
     )
 }
+
 export default CardM;
